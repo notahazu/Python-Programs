bf0ecb7b286ec8cc989c7050ac911a4c47d42416
notahazu <54483162+notahazu@users.noreply.github.com>
2019-08-24 15:18:33 -0400

    Add files via upload

---
+++ GungnirPasswordCracker.py
@@ -0,0 +1,53 @@
+print("Program by not_ahazu.")
+print("Glorious Gungnir Password Cracker")
+print("Please type in Discord Nickname...")
+print("If user changes nickname, please contact author via discord.")
+print("Need help? Type I for Info!")
+print("To exit program, type Close")
+if input() =="Close":
+    import sys
+    sys.exit()
+if input() == "Zarozian":
+    print("NewYorkPhilosopher43")
+if input() == "Cephalon Apple":
+            print("iHEARTzaro")
+
+if input() == "Uzaki-dono":
+    print("HentaiGuy132")
+
+if input() == "Ching Chang the Ding Dong-Man":
+    print("Germany1933")
+
+if input() == "NotPantanium":
+    print("OstrichLover69")
+
+if input() == "Soy Boy Oni":
+    ("Error : Its somewhere in the NSFW Hentai channel...")
+
+if input() == "NeyMey":
+    print("LooksLikeShiroFromSenkoSan")
+
+if input() == "ShadowMirri":
+    print("PlayingBlackDesertOnlineFor16Hours")
+    print("not_ahazu says, Dude get a life lol")
+
+if input() == "KettleTicket":
+    print("Tuna's_Older_Brother")
+
+if input() == "MajesticTuna":
+    print("System Says : Its most likely too gross to say.")
+    print("not_ahazu say's, https://www.youtube.com/watch?v=DWtpNPZ4tb4")
+if input() == "I":
+    print("The Users are...")
+    print("Zarozian")
+    print("Cephalon Apple")
+    print("Uzaki-dono")
+    print("Ching Chang the Ding Dong-Man")
+    print("NotPantanium")
+    print("Soy Boy Oni")
+    print("NeyMey")
+    print("ShadowMirri")
+    print("KettleTicket")
+    print("MajesticTuna")
+    print("This project is a joke. This does not show real passwords.")
+                                                                                 
